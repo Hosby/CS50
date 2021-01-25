3098f7b5fd2a27f2516c733c5ac080f27d04e74d
Hosb <70309863+Hosby@users.noreply.github.com>
2021-01-25 22:24:37 +0900

    Create scores2.c

---
+++ scores2.c
@@ -0,0 +1,36 @@
+/*
+  사용자에게 점수의 갯수를 입력받고, 사용자에게 점수의 갯수만큼 점수를 입력할 수 있게 구현했다.
+  그리고 그 세개의 평균을 출력하도록 작성함.
+*/
+
+#include <cs50.h>
+#include <stdio.h>
+
+float average(int length, int array[]);
+
+int main(void)
+{
+    // 사용자로부터 점수의 갯수 입력
+    int n = get_int("Scores:  ");
+
+    // 점수 배열 선언 및 사용자로부터 값 입력
+    int scores[n];
+    for (int i = 0; i < n; i++)
+    {
+        scores[i] = get_int("Score %i: ", i + 1);
+    }
+
+    // 평균 출력
+    printf("Average: %.1f\n", average(n, scores));
+}
+
+//평균을 계산하는 함수
+float average(int length, int array[])
+{
+    int sum = 0;
+    for (int i = 0; i < length; i++)
+    {
+        sum += array[i];
+    }
+    return (float) sum / (float) length;
+}
